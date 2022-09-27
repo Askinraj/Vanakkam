6a2f64617479b38d7a630fba58a2a22d853e1d2f
Itachi Uchiha <askinraj@gmail.com>
2022-09-27 17:11:57 +0530

    firstcommit

---
+++ Office.java
@@ -0,0 +1,22 @@
+package tamilnadu.chennai; 
+//javac -d . Office.java
+public class Office // package private
+{
+void allowance()
+{
+System.out.println("daily travel allowence=100");
+}
+public void develop()
+{
+System.out.println("Development"); 
+}
+public void test()
+{
+System.out.println("Testing"); 
+}
+protected void do_project()
+{
+System.out.println("project strategies");
+}
+
+}
